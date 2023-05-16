78ac13d66aff21baeaf458ea09fd2b2e014f34f3
Ghianda22 <valelore98@gmail.com>
2023-05-16 10:43:03 +0200

    Translated the whole bowling game to typescript

--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,82 @@
-export class Game {
-    private _score: number = 0;
+const MAX_POINTS_PER_ROLL: number = 10;
+const MAX_ROLLS_PER_GAME: number = 21;
 
+export class Game {
+    _score: number = 0;
+    _rollsInGame: number[] = [];
+
+    /**
+     * This method calculates the bonuses of the game and returns the final score.
+     * <br>
+     * Bonuses:
+     * <br>
+     * - Spare: when the player knocks down all MAX_POINTS_PER_ROLL pins in two rolls
+     * <br>
+     * -> bonus = number of pins of the next roll
+     * <br>
+     * - Strike: when the player knocks down all MAX_POINTS_PER_ROLL pins in the first roll of the frame
+     * <br>
+     * -> bonus = number of pins of the next two rolls
+     * <br>
+     *
+     * @return the final score of the game with bonuses
+     */
     public score(): number {
+        for (let i = 0; i < this._rollsInGame.length; i += 2) {
+            if (this._rollsInGame.length > i + 3) {
+
+                let firstRollAfterCurrentFrame: number = this._rollsInGame[i + 2];
+
+                if (this.isNotSpare(i)) {
+                    continue;
+                }
+
+                this._score += firstRollAfterCurrentFrame;
+
+                if (this.isNotStrike(i)) {
+                    continue;
+                }
+
+                if (firstRollAfterCurrentFrame !== MAX_POINTS_PER_ROLL) {
+                    this._score += this._rollsInGame[i + 3];
+                } else {
+                    this._score += this._rollsInGame[i + 4];
+                }
+            }
+        }
         return this._score;
+    }
+
+
+    /**
+     * This method is called after each roll.
+     * It saves the roll in the list and sums it to the partial score
+     */
+    roll(knockedDownPins: number) {
+        this._rollsInGame.push(knockedDownPins);
+
+        if(knockedDownPins == MAX_POINTS_PER_ROLL && this.isNotLastFrame() && this.isFirstRollOfFrame(knockedDownPins)){
+            this._rollsInGame.push(0);
+        }
+
+        this._score += knockedDownPins;
+
+    }
+
+
+    isNotSpare(i: number): boolean {
+        return (this._rollsInGame[i] + this._rollsInGame[i + 1]) !== MAX_POINTS_PER_ROLL;
+    }
+
+    isNotStrike(i: number): boolean {
+        return this._rollsInGame[i] !== MAX_POINTS_PER_ROLL;
+    }
+
+    isNotLastFrame(): boolean {
+        return this._rollsInGame.length < MAX_ROLLS_PER_GAME - 3;
+    }
+
+    isFirstRollOfFrame(knockedDownPins: number): boolean {
+        return this._rollsInGame.lastIndexOf(knockedDownPins) % 2 == 0;
     }
 }
--- tests/game.test.ts
+++ tests/game.test.ts
@@ -1,14 +1,215 @@
 import {Game} from "../src/game";
-import {expect, test} from "@jest/globals";
+import {describe, expect, it, test} from "@jest/globals";
 
-const game = new Game();
+const getRandomIntFrom0ToBound = (bound: number): number => Math.floor(Math.random() * bound);
+
 
 test('givenEmptyGame_thenScoreShouldReturn0', () => {
     //given
+    const game = new Game();
+
     //when
     const actualScore: number = game.score();
 
     //then
     expect(actualScore).toBe(0);
+})
+
+test('givenEmptyGame_whenCallingRollX_thenScoreShouldReturnX', () => {
+    //given
+    const game = new Game();
+
+    const x: number = getRandomIntFrom0ToBound(11);
+
+    //when
+    game.roll(x);
+    const actualScore: number = game.score();
+
+    //then
+    expect(actualScore).toBe(x);
+})
+
+test('givenGameWithNoSparesNorStrikes_thenScoreShouldReturnSummedRolls', () => {
+    //given
+    const game = new Game();
+
+    let rolls: number [] = [];
+    let expectedScore: number = 0;
+
+    for (let i = 0; i < 10; i += 2) { //incrementing of 2 to pass from a frame to other
+
+        //defining the rolls for the frame, assuring the sum never get to 10
+        rolls[i] = getRandomIntFrom0ToBound(10);
+        rolls[i + 1] = getRandomIntFrom0ToBound(10 - rolls[i]);
+
+        //calculating the expected score
+        expectedScore += rolls[i];
+        expectedScore += rolls[i + 1];
+
+        //assigning the rolls to the game
+        game.roll(rolls[i]);
+        game.roll(rolls[i + 1]);
+    }
+
+
+    //when
+    const actualScore = game.score();
+
+    //then
+    expect(actualScore).toBe(expectedScore);
+})
+
+
+describe('givenGameWithSpare_thenScoreShouldDoubleTheRollAfterSpare', () => {
+    it.each([
+        [[8, 2, 2, 4, 5, 4, 3, 3, 2, 3, 2, 5, 7, 0, 0, 1, 5, 1, 2, 6], 67],
+        [[2, 8, 7, 2, 8, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 63],
+        [[5, 5, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4], 96],
+        [[9, 1, 3, 2, 4, 1, 1, 7, 5, 3, 0, 0, 6, 1, 3, 0, 2, 7, 9, 0], 67]
+    ])("", (rolls: number[], expectedScore: number) => {
+        //given
+        const game = new Game();
+
+        for (const knockedDownPins of rolls) {
+            game.roll(knockedDownPins);
+        }
+
+        //when
+        const actualScore = game.score();
+
+
+        //then
+        // expect(actualScore).toBe(expectedScore);
+    })
+})
+
+describe('givenGameWithStrike_thenScoreShouldDoubleTheTwoRollsAfterStrike', () => {
+    it.each([
+        [[8, 2, 2, 4, 5, 4, 3, 3, 10, 2, 5, 7, 0, 0, 1, 5, 1, 2, 6], 79],
+        [[2, 8, 7, 2, 8, 1, 2, 2, 10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 73],
+        [[5, 5, 5, 4, 5, 4, 5, 4, 10, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4], 106],
+        [[9, 1, 3, 2, 4, 1, 1, 7, 10, 0, 0, 6, 1, 3, 0, 2, 7, 9, 1], 70]
+    ])("", (rolls: number[], expectedScore: number) => {
+        //given
+        const game = new Game();
+
+        for (const knockedDownPins of rolls) {
+            game.roll(knockedDownPins);
+        }
+
+        //when
+        const actualScore = game.score();
+
+        //then
+        expect(actualScore).toBe(expectedScore);
+    })
+})
+
+describe('givenGameWithSpareInLastFrame_thenScoreShouldIncludeTheBonusRoll', () => {
+    it.each([
+        [[8, 2, 2, 4, 5, 4, 3, 3, 10, 2, 5, 7, 0, 0, 1, 5, 1, 4, 6, 4], 85],
+        [[2, 8, 7, 2, 8, 1, 2, 2, 10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8, 3], 82],
+        [[5, 5, 5, 4, 5, 4, 5, 4, 10, 5, 4, 5, 4, 5, 4, 5, 4, 5, 5, 6], 113],
+        [[9, 1, 3, 2, 4, 1, 1, 7, 10, 0, 0, 6, 1, 3, 0, 2, 7, 9, 1, 8], 78]
+    ])("", (rolls: number[], expectedScore: number) => {
+        //given
+        const game = new Game();
+
+        for (const knockedDownPins of rolls) {
+            game.roll(knockedDownPins);
+        }
+
+        //when
+        const actualScore = game.score();
+
+        //then
+        expect(actualScore).toBe(expectedScore);
+    })
+})
+
+describe('givenGameWithStrikeInLastFrame_thenScoreShouldIncludeTheBonusRoll', () => {
+    it.each([
+        [[8, 2, 2, 4, 5, 4, 3, 3, 10, 2, 5, 7, 0, 0, 1, 5, 1, 10, 6, 4], 91],
+        [[2, 8, 7, 2, 8, 1, 2, 2, 10, 2, 2, 2, 2, 2, 2, 2, 2, 10, 8, 3], 90],
+        [[5, 5, 5, 4, 5, 4, 5, 4, 10, 5, 4, 5, 4, 5, 4, 5, 4, 10, 5, 6], 118],
+        [[9, 1, 3, 2, 4, 1, 1, 7, 10, 0, 0, 6, 1, 3, 0, 2, 7, 10, 1, 8], 79]
+    ])("", (rolls: number[], expectedScore: number) => {
+        //given
+        const game = new Game();
+
+        for (const knockedDownPins of rolls) {
+            game.roll(knockedDownPins);
+        }
+
+        //when
+        const actualScore = game.score();
+
+        //then
+        expect(actualScore).toBe(expectedScore);
+    })
+})
+
+describe('givenGameWithAllSpares_thenScoreShouldIncludeBonusRolls', () => {
+    it.each([
+        [[9, 1, 2, 8, 0, 10, 1, 9, 9, 1, 1, 9, 8, 2, 4, 6, 2, 8, 4, 6, 3],134],
+        [[6, 4, 0, 10, 3, 7, 8, 2, 7, 3, 6, 4, 3, 7, 4, 6, 4, 6, 5, 5, 8],148],
+        [[5, 5, 4, 6, 4, 6, 4, 6, 7, 3, 5, 5, 4, 6, 5, 5, 4, 6, 1, 9, 0],138],
+        [[4, 6, 6, 4, 1, 9, 0, 10, 8, 2, 1, 9, 4, 6, 9, 1, 8, 2, 5, 5, 5],147]
+    ])("", (rolls: number[], expectedScore: number) => {
+        //given
+        const game = new Game();
+
+        for (const knockedDownPins of rolls) {
+            game.roll(knockedDownPins);
+        }
+
+        //when
+        const actualScore = game.score();
+
+        //then
+        expect(actualScore).toBe(expectedScore);
+    })
+})
+
+
+test('givenGameWithAllSpares_thenScoreShouldIncludeBonusRolls', () => {
+    //given
+    const game = new Game();
+    let expectedScore: number = 100;  //since frames are 10 and there's a spare in each frame, the score it's a minimum of 100
+    for (let i = 0; i < 10; i++) {
+        const firstRoll: number = getRandomIntFrom0ToBound(10); //it cant' score 10 or it'd be a strike
+        const secondRoll: number = 10 - firstRoll;
+        game.roll(firstRoll);
+        game.roll(secondRoll);
+        if (i > 0) {
+            expectedScore += firstRoll;
+        }
+    }
+
+    const extraRoll = getRandomIntFrom0ToBound(11);
+    game.roll(extraRoll);
+    expectedScore += extraRoll;
+
+    //when
+    const actualScore = game.score();
+
+    //then
+    expect(actualScore).toBe(expectedScore);
+
+})
+
+test('givenGameWithAllSpares_thenScoreShouldIncludeBonusRolls', () => {
+    //given
+    const game = new Game();
+    for (let i = 0; i < 12; i++) {
+        game.roll(10);
+    }
+
+    //when
+    const actualScore = game.score();
+
+    //then
+    expect(actualScore).toBe(300);
+
 })
 
